dec9f7a7cdad81b14e1bf3b9848acbc3f0b8c06a
Anthony <the.statistics.network@gmail.com>
2019-03-30 22:34:57 +1100

    yay more cheat-scripts :)

---
+++ useful-scripts/Rmarkdown-cheats.R
@@ -0,0 +1,12 @@
+captions in rmarkdown::all_output_formats(```{r, fig.cap = cap}
+                                          ## Plot 1
+                                          qplot(carat, price, data = diamonds)
+                                          cap <- "This is caption 1"
+                                          
+                                          ## Plot 2
+                                          qplot(carat, depth, data = diamonds)
+                                          
+                                          cap <- c(cap, "This is caption 2")
+                                          
+                                          ```
+                                          )
