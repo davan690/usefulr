8856bf87ac901601bc1d290943bd4e40d57ab216
Anthony <the.statistics.network@gmail.com>
2019-04-04 18:30:58 +1100

    tibles me

---
+++ useful-scripts/Tibble-fun.R
@@ -0,0 +1,11 @@
+# Tibble FUN
+# April 2019
+# Anthony Davidson
+
+tibble(
+  a = lubridate::now() + runif(1e3) * 86400,
+  b = lubridate::today() + runif(1e3) * 30,
+  c = 1:1e3,
+  d = runif(1e3),
+  e = sample(letters, 1e3, replace = TRUE)
+)
