c89f23032cc818eed3facc43c118ddddb4297000
Anthony <the.statistics.network@gmail.com>
2019-05-13 17:02:08 +1000

    ggplot scripts

--- useful-scripts/ggplot2-theme-mods.R
+++ useful-scripts/ggplot2-theme-mods.R
@@ -1,3 +1,41 @@
+# theme mods
+# May2019
+# Anthony Davidson
+
+# theme mods
+# generating new theme
+theme_new <- function(base_size = 11,
+                      base_family = "",
+                      base_line_size = base_size / 170,
+                      base_rect_size = base_size / 170){
+  theme_minimal(base_size = base_size, 
+                base_family = base_family,
+                base_line_size = base_line_size) %+replace%
+    theme(
+      plot.title = element_text(
+        color = rgb(25, 43, 65, maxColorValue = 255), 
+        face = "bold",
+        hjust = 0),
+      axis.title = element_text(
+        color = rgb(105, 105, 105, maxColorValue = 255),
+        size = rel(0.75)),
+      axis.text = element_text(
+        color = rgb(105, 105, 105, maxColorValue = 255),
+        size = rel(0.5)),
+      panel.grid.major = element_line(
+        rgb(105, 105, 105, maxColorValue = 255),
+        linetype = "dotted"),   
+      panel.grid.minor = element_line(
+        rgb(105, 105, 105, maxColorValue = 255),
+        linetype = "dotted", 
+        size = rel(4)),   
+      
+      complete = TRUE
+    )
+}
+
+#comparing results
+
 # base plot
 base_plot <- data.frame(x = rnorm(n = 100, 1.5, 2),
                         y = rnorm(n = 100, 1, 2),
